fb238933cdec9b8fccdd5dde53756bd9ba137e64
revoke0699 <46583229+revoke0699@users.noreply.github.com>
2024-09-25 11:30:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.0220306.xyz": dockerHub,
+  "quay.0220306.xyz": "https://quay.io",
+  "gcr.0220306.xyz": "https://gcr.io",
+  "k8s-gcr.0220306.xyz": "https://k8s.gcr.io",
+  "k8s.0220306.xyz": "https://registry.k8s.io",
+  "ghcr.0220306.xyz": "https://ghcr.io",
+  "cloudsmith.0220306.xyz": "https://docker.cloudsmith.io",
+  "ecr.0220306.xyz": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.0220306.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
